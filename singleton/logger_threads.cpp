fe9f0711049ad4845e72661f0ac9970b0c3f107d
Nishant Bisht <116999283+a3s0h@users.noreply.github.com>
2023-12-02 19:59:36 +0530

    added locks

--- singleton/logger_threads.cpp
+++ singleton/logger_threads.cpp
@@ -2,27 +2,34 @@
 #include<iostream>
 #include<memory>
 #include<thread>
-
+#include<mutex>
 
 class Logger 
 {
     static int ctr;
     static std::unique_ptr<Logger> loggerInstance;
-
+    static std::mutex mtx;
     Logger()
     {
         ctr++;
         std::cout << "New instance created: no of instances: " << ctr << std::endl;
     }
+    Logger(const Logger&);     // we are restricting copy constructor and ..
+    Logger operator=(const Logger &);     //    ..= operator overloading from user therefore we made them private
 
 public:
     // Now we cannot create objects as the constructor is private, therefore we will make a static function which will 
     // be creating the instance 
     static Logger* getLogger()
     {
-        if (loggerInstance == nullptr)  // if the instance is not created then create one 
+        if(loggerInstance == nullptr)       // we are doing double check because locks are expensive
         {
-            loggerInstance = std::unique_ptr<Logger>(new Logger());
+            mtx.lock();
+            if (loggerInstance == nullptr)  // if the instance is not created then create one 
+            {
+                loggerInstance = std::unique_ptr<Logger>(new Logger());
+            }
+            mtx.unlock();
         }
         return loggerInstance.get();  // returning a raw pointer
     }
@@ -35,7 +42,7 @@ public:
 
 int Logger::ctr = 0;           //  (type belongs to::variable_name = value)
 std::unique_ptr<Logger> Logger::loggerInstance = nullptr;
-
+std::mutex Logger::mtx ;
 
 
 void user1Log()
